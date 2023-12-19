e3a5758a4fb899b89fdd4105d67b54bb88d81103
Jake Johnson <jkjohnson1996@gmail.com>
2023-12-19 10:57:05 -0800

    background image

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,49 +1,54 @@
 import React, { useState } from "react";
-import MonthlyView from './MonthlyView';
-import MeetingDetails from './MeetingDetails';
-import WeeklyView from './WeeklyView';
+import MonthlyView from "./MonthlyView";
+import MeetingDetails from "./MeetingDetails";
+import WeeklyView from "./WeeklyView";
 import DisplayArea from "./DisplayArea";
-import './style/MainComponent.css'
+import "./style/MainComponent.css";
 
 const MainComponent = () => {
-    const [startDate, setStartDate] = useState(null);
-    const [endDate, setEndDate] = useState(null);
-    const [meetingName, setMeetingName] = useState('');
-    const [meetingDescription, setMeetingDescription] = useState('');
-    const [meetingLocation, setMeetingLocation] = useState('');
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [meetingName, setMeetingName] = useState("");
+  const [meetingDescription, setMeetingDescription] = useState("");
+  const [meetingLocation, setMeetingLocation] = useState("");
 
-    const handleDefineMeeting = (name, description, location) => {
-        setMeetingName(name);
-        setMeetingDescription(description);
-        setMeetingLocation(location);
-    };
+  const handleDefineMeeting = (name, description, location) => {
+    setMeetingName(name);
+    setMeetingDescription(description);
+    setMeetingLocation(location);
+  };
 
-    return (
-        <div className="mainContainer">
-            <div className="leftContainer">
-                <MonthlyView setStartDate={setStartDate} setEndDate={setEndDate} startDate={startDate} endDate={endDate} />
-                <hr />
-                <MeetingDetails
-                    onDefineMeeting={handleDefineMeeting}
-                    meetingName={meetingName}
-                    meetingDescription={meetingDescription}
-                    meetingLocation={meetingLocation}
-                    setMeetingName={setMeetingName}
-                    setMeetingDescription={setMeetingDescription}
-                    setMeetingLocation={setMeetingLocation}
-                />
-                <hr />
-                <WeeklyView startDate={startDate} endDate={endDate} />
-            </div>
-            <div className="rightContainer">
-                <DisplayArea 
-                    meetingName={meetingName} 
-                    meetingDescription={meetingDescription} 
-                    meetingLocation={meetingLocation} 
-                />
-            </div>
-        </div>
-    )
+  return (
+    <div className="mainContainer">
+      <div className="leftContainer">
+        <MonthlyView
+          setStartDate={setStartDate}
+          setEndDate={setEndDate}
+          startDate={startDate}
+          endDate={endDate}
+        />
+        <hr />
+        <MeetingDetails
+          onDefineMeeting={handleDefineMeeting}
+          meetingName={meetingName}
+          meetingDescription={meetingDescription}
+          meetingLocation={meetingLocation}
+          setMeetingName={setMeetingName}
+          setMeetingDescription={setMeetingDescription}
+          setMeetingLocation={setMeetingLocation}
+        />
+        <hr />
+        <WeeklyView startDate={startDate} endDate={endDate} />
+      </div>
+      <div className="rightContainer">
+        <DisplayArea
+          meetingName={meetingName}
+          meetingDescription={meetingDescription}
+          meetingLocation={meetingLocation}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default MainComponent;
--- src/components/MonthlyView.jsx
+++ src/components/MonthlyView.jsx
@@ -5,6 +5,8 @@ import { LocalizationProvider, DateCalendar } from '@mui/x-date-pickers';
 import './style/MonthlyView.css';
 
 const MonthlyView = ({ setStartDate, setEndDate, startDate, endDate }) => {
+  console.log(startDate);
+  console.log(endDate);
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -49,3 +51,86 @@ export default MonthlyView;
 //     </LocalizationProvider>
 //   );
 // }
+
+
+/* Multiple date picker
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import StaticDatePicker from "@mui/lab/StaticDatePicker";
+import PickersDay from "@mui/lab/PickersDay";
+import startOfDay from "date-fns/startOfDay";
+
+const CustomPickersDay = styled(PickersDay, {
+  shouldForwardProp: (prop) => prop !== "selected"
+})(({ theme, selected }) => ({
+  ...(selected && {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+    "&:hover, &:focus": {
+      backgroundColor: theme.palette.primary.dark
+    },
+    borderTopLeftRadius: "50%",
+    borderBottomLeftRadius: "50%",
+    borderTopRightRadius: "50%",
+    borderBottomRightRadius: "50%"
+  })
+}));
+
+export default function MultiDatePicker() {
+  const [values, setValues] = React.useState([startOfDay(new Date())]);
+
+  const findDate = (dates, date) => {
+    const dateTime = date.getTime();
+    return dates.find((item) => item.getTime() === dateTime);
+  };
+
+  const findIndexDate = (dates, date) => {
+    const dateTime = date.getTime();
+    return dates.findIndex((item) => item.getTime() === dateTime);
+  };
+
+  const renderPickerDay = (date, selectedDates, pickersDayProps) => {
+    if (!values) {
+      return <PickersDay {...pickersDayProps} />;
+    }
+
+    const selected = findDate(values, date);
+
+    return (
+      <CustomPickersDay
+        {...pickersDayProps}
+        disableMargin
+        selected={selected}
+      />
+    );
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <StaticDatePicker
+        displayStaticWrapperAs="desktop"
+        label="Week picker"
+        value={values}
+        onChange={(newValue) => {
+          //copying the values array 
+          const array = [...values];
+          const date = startOfDay(newValue);
+          const index = findIndexDate(array, date);
+          if (index >= 0) {
+            array.splice(index, 1);
+          } else {
+            array.push(date);
+          }
+          setValues(array);
+        }}
+        renderDay={renderPickerDay}
+        renderInput={(params) => <TextField {...params} />}
+        inputFormat="'Week of' MMM d"
+      />
+    </LocalizationProvider>
+  );
+}
+*/
